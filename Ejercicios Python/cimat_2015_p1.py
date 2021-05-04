b3c28c36ae1323016b2517900d6bb5dd91c4e264
Josue <josue@vauxoo.com>
2021-05-04 14:12:20 -0500

    [REF] cimat_2015_p1: Add the code in a function

--- Ejercicios Python/cimat_2015_p1.py
+++ Ejercicios Python/cimat_2015_p1.py
@@ -1,22 +1,25 @@
 """
-Se tiene un arreglo de caracteres C y se sabe que el ´ultimo elemento de C es el ´unico que contiene un
-cero. Dar el c´odigo para contar las ocurrencias aisladas del car´acter ’a’ (es decir, cuando los caracteres
+Se tiene un arreglo de caracteres C y se sabe que el ´ultimo elemento de C es el único que contiene un
+cero. Dar el código para contar las ocurrencias aisladas del carácter ’a’ (es decir, cuando los caracteres
 vecinos a una ’a’ no sean ’a’s).
 
-Ejemplo: ante la ejecuci´on de la funci´on con la siguiente cadena de entrada:
+Ejemplo: ante la ejecución de la funci´on con la siguiente cadena de entrada:
 {a, d, a, a, f, a, g, r, r, a, 0},
 se debe devolver 3
 """
 
+def obtener_ocurrencias(lista_caracteres):
+    contador_ocurrencias = 0
+
+    for datos in range(len(lista_caracteres)-1):
+        if datos == 0:
+            if lista_caracteres[0] == 'a' and lista_caracteres[1] != 'a':
+                contador_ocurrencias += 1
+        else:
+            if lista_caracteres[datos-1] != 'a' and lista_caracteres[datos] == 'a' and lista_caracteres[datos+1] != 'a':
+                contador_ocurrencias += 1
+
+    print(contador_ocurrencias)
+
 lista = ['a','a','a','f','a','h','a','r','r','a','0']
-contador_ocurrencias = 0
-
-for datos in range(len(lista)-1):
-    if datos == 0:
-     if lista[0] == 'a' and lista[1] != 'a':
-         contador_ocurrencias += 1
-    else:
-        if lista[datos-1] != 'a' and lista[datos] == 'a' and lista[datos+1] != 'a':
-            contador_ocurrencias += 1
-
-print(contador_ocurrencias)
+obtener_ocurrencias(lista)
