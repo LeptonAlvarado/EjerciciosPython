1fc59d552e130dc9dfaaaff48fd012dc590b149d
Josue <josue@vauxoo.com>
2021-05-05 01:08:25 -0500

    [ADD] cimat_2015_p3.py: Add code for validate my answer

---
+++ Ejercicios Python/cimat_2015_p3.py
@@ -0,0 +1,15 @@
+"""
+En el c ́odigo siguiente, ¿cu ́al es el valor deial final de la ejecución?
+
+R = 5
+"""
+
+x = 1
+i = 1
+
+while(x <= 1000):
+    x = pow(2, x)
+    print(x)
+    i = i + 1
+
+print(i)
