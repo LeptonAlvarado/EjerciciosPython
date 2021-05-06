1cfa1962c0aab5964d17e222f124b4ccc9b84465
Josue <josue@vauxoo.com>
2021-05-05 20:03:38 -0500

    [REF] cimat_2015_p2: Make a general function for all the cases

--- Ejercicios Python/cimat_2015_p2.py
+++ Ejercicios Python/cimat_2015_p2.py
@@ -3,9 +3,9 @@ Implemente una funci ́onvoid SumDiagonals(int data[N][M], int result[])que calc
 una de las diagonales cuya orientaci ́on es hacia la derecha y abajo, en la matriz data, y almacenelos
 resultados en el array result.
 Ejemplo: ante la ejecuci ́on de la funci ́on con la siguiente matriz data:
-[10   26
-12    20
-−5    4]
+[10   26    3
+12    20    5
+−5    4     2]
 la funci ́on debe almacenar en result el siguiente contenido:{26,30,16,−5}
 """
 lista_data = [[10,26], [12,20], [-5,4]]
@@ -13,7 +13,12 @@ lista_data = [[10,26], [12,20], [-5,4]]
 def sumDiagonals(data, result):
     fila = 0
     indice_f = 0
+    lst =[]
+    for i in range(len(data[0])):
+        lst.append(0)
+    data.append(lst)
     for row in data:
+        indice_c = len(data)-1
         for elemt in range(len(row)):
             indice_f = fila -1
             if indice_f < 0 and elemt != 0:
@@ -22,9 +27,6 @@ def sumDiagonals(data, result):
                 suma = data[fila-1][elemt-1] + data[fila][elemt]
                 result.append(suma)
         fila += 1
-
-    result.append(data[len(data)-1][0])
-
     print(result)
 
 sumDiagonals(lista_data, result=[])
